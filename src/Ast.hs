c0b14d23b4f7582737714fed51704af470dff6c5
zjhmale <zjhmale@gmail.com>
2016-04-27 13:30:33 +0800

    add basic syntax

---
+++ src/Ast.hs
@@ -0,0 +1,39 @@
+module Ast where
+
+import qualified Text.PrettyPrint as PP
+
+type Name = String
+
+data Term = TTrue
+          | TFalse
+          | TVar Name
+          | TNeg Term
+          | TConj Term Term
+          | TDisj Term Term
+          | TImpl Term Term
+          | TEquiv Term Term
+          | TForall Name Term
+          | TExists Name Term
+
+infix 1 |>
+
+(|>) :: a -> (a -> b) -> b
+(|>) = flip ($)
+
+parens :: String -> String
+parens s = "(" ++ s ++ ")"
+
+instance Show Term where
+  showsPrec _ x = shows $ PP.text $ prType x
+
+prType :: Term -> String
+prType TTrue = "TRUE"
+prType TFalse = "FALSE"
+prType (TVar n) = n
+prType (TNeg t) = "¬" ++ prType t |> parens
+prType (TConj t1 t2) = prType t1 ++ " ∧ " ++ prType t2 |> parens
+prType (TDisj t1 t2) = prType t1 ++ " ∨ " ++ prType t2 |> parens
+prType (TImpl t1 t2) = prType t1 ++ " ⇒ " ++ prType t2 |> parens
+prType (TEquiv t1 t2) = prType t1 ++ " ≡ " ++ prType t2 |> parens
+prType (TForall n t) = "∀" ++ n ++ ". " ++ prType t |> parens
+prType (TExists n t) = "∃" ++ n ++ ". " ++ prType t |> parens
