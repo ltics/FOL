4fdb4d31bfccab888ea52524d1efe6acaf79388e
zjhmale <zjhmale@gmail.com>
2016-04-27 22:15:21 +0800

    add basic evaluation

---
+++ src/Core.hs
@@ -0,0 +1,30 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Core where
+
+import Ast
+
+-- use closure to mock envrionment
+
+empty :: Show a => a -> t
+empty x = error $ "Undefined variable: " ++ show x
+
+extend :: Eq a => (a -> t) -> a -> t -> a -> t
+extend f k v = \x -> if x == k then v else f x
+
+eval :: (Name -> Bool) -> Term -> Bool
+eval env term = case term of
+                  TTrue -> True
+                  TFalse -> False
+                  TVar n -> env n
+                  TNeg t -> not $ eval env t
+                  TConj t1 t2 -> (eval env t1) && (eval env t2)
+                  TDisj t1 t2 -> (eval env t1) || (eval env t2)
+                  TImpl t1 t2 -> not (eval env t1) || (eval env t2)
+                  TEquiv t1 t2 -> (eval env t1) == (eval env t2)
+                  TForall n t -> e1 && e2 where
+                    e1 = eval (extend env n True) t
+                    e2 = eval (extend env n False) t
+                  TExists n t -> e1 || e2 where
+                    e1 = eval (extend env n True) t
+                    e2 = eval (extend env n False) t
